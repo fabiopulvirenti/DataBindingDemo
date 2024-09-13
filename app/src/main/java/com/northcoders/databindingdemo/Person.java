335ef02e85a2887c7fe72cbc4c1339c3d517951a
fabiopulvirenti <fpulvirenti002@gmail.com>
2024-09-13 12:01:37 +0100

    feat:task three completed

---
+++ app/src/main/java/com/northcoders/databindingdemo/Person.java
@@ -0,0 +1,34 @@
+package com.northcoders.databindingdemo;
+
+public class Person {
+
+    String name;
+
+    int age;
+
+    String email;
+
+    public Person(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
