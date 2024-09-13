2965d2dbf85b857e77d325e8862368088fb464b7
Reece Moore <reecemoore@192.168.1.113>
2024-09-13 15:02:18 +0100

    Completed task 10

--- app/src/main/java/com/northcoders/databindingdemo/MainActivity.java
+++ app/src/main/java/com/northcoders/databindingdemo/MainActivity.java
@@ -7,18 +7,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.northcoders.databindingdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Person person = new Person("Reece", 36, "reece@example.com");
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        activityMainBinding.setPerson(person);
     }
 }
--- app/src/main/java/com/northcoders/databindingdemo/Person.java
+++ app/src/main/java/com/northcoders/databindingdemo/Person.java
@@ -1,12 +1,15 @@
 package com.northcoders.databindingdemo;
 
-public class Person {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
 
-    String name;
+public class Person extends BaseObservable {
 
-    int age;
+    private String name;
 
-    String email;
+    private int age;
+
+    private String email;
 
     public Person(String name, int age, String email) {
         this.name = name;
@@ -14,21 +17,33 @@ public class Person {
         this.email = email;
     }
 
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
+    @Bindable
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
         this.age = age;
+        notifyPropertyChanged(BR.age);
     }
 
+    @Bindable
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+        notifyPropertyChanged(BR.email);
+    }
 }
